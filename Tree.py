917527e3cc5d5a980b18e8ba376779002550a70e
AMazur2 <45732226+AMazur2@users.noreply.github.com>
2021-01-26 20:46:04 +0100

    Update Tree.py

--- Tree.py
+++ Tree.py
@@ -4,71 +4,78 @@ import pandas as pd
 
 class Tree:
 
-    def __init__(self, labelColumn: int):
-        self.labelColumn = labelColumn
+    def __init__(self):
+        pass
 
     # We check here whether our data set contains only data from one class
-    def isPure(self, data) -> bool:
-        label = data[:, self.labelColumn]
+    def isPure(self, data, label) -> bool:
         uniques = np.unique(label)
         if len(uniques) == 1:
             return True
         return False
 
     # This function will be called only when data is pure, so we can return the classification of this data set
-    def classify(self, data):
-        label = data[:, self.labelColumn]
-        classification = label[0]
-        return classification
+    def classify(self, data, label):
+        l = label[0]
+        return l[0]
 
     # Find potential splits
     def potentialSplits(self, data):
         splits = {}
         _, numberOfColumns = data.shape
         for column_id in range(numberOfColumns):
-            if column_id != self.labelColumn:
-                splits[column_id] = []
-                values = data[:, column_id]
-                unique = np.sort(np.unique(values))
+            splits[column_id] = []
+            values = data[:, column_id]
+            unique = np.sort(np.unique(values))
 
-                for index in range(len(unique)):
-                    if index != 0:
-                        currentVal = unique[index]
-                        previousVal = unique[index - 1]
-                        splits[column_id].append(float((currentVal + previousVal) / 2))
+            for index in range(len(unique)):
+                if index != 0:
+                    currentVal = unique[index]
+                    previousVal = unique[index - 1]
+                    splits[column_id].append(float((currentVal + previousVal) / 2))
 
         return splits
 
     # Split data by column and value in it
-    def split(self, data, column, value):
+    def split(self, data, label, column, value):
+        label_above = []
+        label_below = []
         valuesInColumn = data[:, column]
-        return data[valuesInColumn <= value], data[valuesInColumn > value]
+        i = 0
+        for featureValue in valuesInColumn:
+            if featureValue <= value:
+                label_below.append(label[i])
+            else:
+                label_above.append(label[i])
+            i = i + 1
+
+        return data[valuesInColumn <= value], label_below, data[valuesInColumn > value], label_above
 
     # Calculate entropy
-    def entropy(self, data):
-        label = data[:, self.labelColumn]
+    def entropy(self, data, label):
         _, counts = np.unique(label, return_counts=True)
         probabilities = counts / counts.sum()
         return sum(probabilities * -np.log2(probabilities))
 
     # calculate overall entropy for split
-    def overallEntropy(self, data_below, data_above):
+    def overallEntropy(self, data_below, label_below, data_above, label_above):
         numberOfPoints = len(data_below) + len(data_above)
 
         probabilityBelow = len(data_below) / numberOfPoints
         probabilityAbove = len(data_above) / numberOfPoints
 
-        return probabilityBelow * self.entropy(data_below) + probabilityAbove * self.entropy(data_above)
+        return probabilityBelow * self.entropy(data_below, label_below) + \
+               probabilityAbove * self.entropy(data_above, label_above)
 
     # find best split
-    def bestSplit(self, data, splits):
+    def bestSplit(self, data, label, splits):
         splitEntropy = 100000
         splitColumn = 0
         splitValue = 0
         for column_id in splits:
             for value in splits[column_id]:
-                below, above = self.split(data, column_id, value)
-                currEntropy = self.overallEntropy(below, above)
+                below, l_below, above, l_above = self.split(data, label, column_id, value)
+                currEntropy = self.overallEntropy(below, l_below, above, l_above)
 
                 if currEntropy < splitEntropy:
                     splitEntropy = currEntropy
@@ -78,27 +85,29 @@ class Tree:
         return splitColumn, splitValue
 
     # Build decision tree where subtree is: {question: [yes_option, no_option]}
-    def buildTree(self, df, level):
+    def buildTree(self, df, labels, level):
         global columnHeaders
         if level == 0:  # we want to give whole dataframe to the algorithm so we must be sure that we have values
             data = df.values
             columnHeaders = df.columns
+            label = labels.values
         else:
             data = df
+            label = labels
 
-        if self.isPure(data):
-            return self.classify(data)
+        if self.isPure(data, label):
+            return self.classify(data, label)
         else:
             level += 1
             splits = self.potentialSplits(data)
-            splitColumn, splitValue = self.bestSplit(data, splits)
-            dataBelow, dataAbove = self.split(data, splitColumn, splitValue)
+            splitColumn, splitValue = self.bestSplit(data, label, splits)
+            dataBelow, label_below, dataAbove, label_above = self.split(data, label, splitColumn, splitValue)
 
             question = "{} <= {}".format(columnHeaders[splitColumn], splitValue)
             subTree = {question: []}
 
-            yes_option = self.buildTree(dataBelow, level)
-            no_option = self.buildTree(dataAbove, level)
+            yes_option = self.buildTree(dataBelow, label_below, level)
+            no_option = self.buildTree(dataAbove, label_above, level)
 
             if yes_option == no_option:
                 subTree[question].append(yes_option)
@@ -123,11 +132,12 @@ class Tree:
             subTree = answer
             return self.tryToClassify(individual, subTree)
 
-    def testTree(self, tree, test_df):
-
+    def testTree(self, tree, test_df, test_label):
+        label_values = test_label.values
+        values = label_values[:, 0]
         correct = 0
         for i in range(len(test_df)):
-            label = test_df.iloc[i][self.labelColumn]
+            label = values[i]
             classification = self.tryToClassify(test_df.iloc[i], tree)
             if classification == label:
                 correct += 1
